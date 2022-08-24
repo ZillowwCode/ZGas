8e3c9501d8528e8aeeba804ea01d5135f862b6c4
Raphaël Branco Vieira <zilloww@pop-os.localdomain>
2022-08-24 13:57:53 +0200

    Prototype of Sign in with Google loading button

--- src/views/Auth/SignInView.vue
+++ src/views/Auth/SignInView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="signin">
     <h1 class="text-h2 mt-4">Connexion</h1>
-    <v-btn color="success" class="mt-10" size="large" @click="signInWithGoogle"><v-icon class="mr-3">mdi-google</v-icon>Connexion avec Google</v-btn>
+    <v-btn color="success" class="mt-10" size="large" @click="signInWithGoogle" :loading="isGoogleSignInLoading"><v-icon class="mr-3">mdi-google</v-icon>Connexion avec Google</v-btn>
   </div>
 </template>
 
@@ -10,9 +10,16 @@ import { googleSignIn } from "@/firebase/services/auth";
 
 export default {
     name: 'SignInView',
+    data() {
+      return {
+        isGoogleSignInLoading: false,
+      }
+    },
     methods: {
       async signInWithGoogle() {
+        this.isGoogleSignInLoading = true;
         await googleSignIn();
+        this.isGoogleSignInLoading = false;
         this.$router.push({ name: 'home' })
       }
     }
