35bac00fbb4642063a191aa796de20882a94634a
Raphaël Branco Vieira <zilloww@pop-os.localdomain>
2022-08-24 14:16:09 +0200

    When a user logs in, create new document in Users collection with Firebase Auth data

--- src/firebase/services/auth.js
+++ src/firebase/services/auth.js
@@ -1,6 +1,7 @@
-import { auth } from "@/firebase/firebase";
+import { auth, db } from "@/firebase/firebase";
 import { useAuthStore } from "@/store/auth";
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { collection, doc, setDoc } from "firebase/firestore";
 
 const googleSignIn = async () => {
     const authStore = useAuthStore();
@@ -9,6 +10,20 @@ const googleSignIn = async () => {
         .then((res) => {
             authStore.setUser(res.user);
         })
+        .catch((err) => {
+            console.error(err);
+        })
+
+    // Add user to Firestore DB
+    const usersCollectionRef = collection(db, "users/");
+    const userDocRef = doc(usersCollectionRef, authStore.user.uid);
+    await setDoc(userDocRef, {
+        id: authStore.user.uid,
+        signupDate: Date.now(),
+        name: authStore.user.displayName,
+        email: authStore.user.email,
+        photoUrl: authStore.user.photoURL
+    }, { merge: true });
 }
 
 const globalSignOut = async () => {
